cfacddb7cb906b41ea13412b907c83fa39074378
Zikrul Bari Tomal <zbtomal.10@gmail.com>
2026-05-04 11:15:15 +0600

    Add: find-the-index-of-the-first-occurrence-in-a-string - submission-0

---
+++ Data Structures & Algorithms/find-the-index-of-the-first-occurrence-in-a-string/submission-0.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        size_t pos = haystack.find(needle);
+        if (pos != string::npos) return pos;
+        else return -1;
+    }
+};
