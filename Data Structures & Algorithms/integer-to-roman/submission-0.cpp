7a97aba648743e8af88f312a896f2f108dda94e9
Zikrul Bari Tomal <zbtomal.10@gmail.com>
2026-05-09 11:34:47 +0600

    Add: integer-to-roman - submission-0

---
+++ Data Structures & Algorithms/integer-to-roman/submission-0.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string intToRoman(int num) {
+        vector<int> value = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
+
+        vector<string> symbol = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
+
+        string ans = "";
+        for (int i = 0; i < value.size(); i++) {
+            while (num >= value[i]) {
+                ans += symbol[i];
+                num -= value[i];
+            }
+        }
+        return ans;
+    }
+};
